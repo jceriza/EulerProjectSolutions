364224767f846b6ed5072c4ace25457d956c1f53
Joaquín Ceriza <joaquin.ceriza@unono.net>
2017-12-21 16:12:39 +0100

    Solving exercise 5

---
+++ Exercises/E5_Smallest_Multiple.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercises
+{
+    public static class E5_Smallest_Multiple
+    {
+        public static long SmallestMultiple(int multipleUntil)
+        {
+            bool validNumber;
+
+            for (int num = multipleUntil; true; num += multipleUntil)
+            {
+                validNumber = true;
+
+                for (int divisor = multipleUntil; divisor > 2; divisor--)
+                {
+                    if (num % divisor != 0)
+                    {
+                        validNumber = false;
+                        break;
+                    }
+                }
+
+                if (validNumber) return num;
+            }
+        }
+    }
+}
---
+++ Exercises_Tests/E5_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E5_Tests
+    {
+        [Fact]
+        public void SmallestMultipleTo10_2520()
+        {
+            var smallestMultiple = E5_Smallest_Multiple.SmallestMultiple(10);
+
+            Assert.Equal(2520, smallestMultiple);
+        }
+
+        [Fact]
+        public void SmallestMultipleTo20_232792560()
+        {
+            var smallestMultiple = E5_Smallest_Multiple.SmallestMultiple(20);
+
+            Assert.Equal(232792560, smallestMultiple);
+        }
+    }
+}
