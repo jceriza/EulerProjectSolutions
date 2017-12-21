b4bd3737acf43220daef9883c3658d1195ddd99c
Joaquín Ceriza <joaquin.ceriza@unono.net>
2017-12-21 16:26:27 +0100

    Solving exercise 7

---
+++ Exercises/E7_10001st_prime.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Utilities;
+
+namespace Exercises
+{
+    public static class E7_10001st_prime
+    {
+        public static long Get10001stPrimeNumber()
+        {
+            return PrimeNumbers.PrimesGeneratorFrom2().Skip(10000).First();
+        }
+    }
+}
---
+++ Exercises_Tests/E7_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E7_Tests
+    {
+        [Fact]
+        public void Get10001stPrimeNumber_104743()
+        {
+            var prime = E7_10001st_prime.Get10001stPrimeNumber();
+
+            Assert.Equal(104743, prime);
+        }
+    }
+}
