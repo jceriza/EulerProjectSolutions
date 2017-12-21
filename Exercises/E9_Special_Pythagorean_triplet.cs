0e325385ee4273d4b65ecbd184c1c7735d943e77
Joaquín Ceriza <joaquin.ceriza@unono.net>
2017-12-21 16:54:39 +0100

    Solving exercise 9

---
+++ Exercises/E9_Special_Pythagorean_triplet.cs
@@ -0,0 +1,20 @@
+﻿using Utilities;
+
+namespace Exercises
+{
+    public static class E9_Special_Pythagorean_triplet
+    {
+        public static int PythagoreanProduct(int desiredSum)
+        {
+            foreach (var (a, b, c) in PythagoreanTriplet.PythagoreanTripletGenerator())
+            {
+                if (a + b + c == desiredSum)
+                {
+                    return a * b * c;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Exercises_Tests/E9_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E9_Tests
+    {
+        [Fact]
+        public void SpecialPythagoreanTriplet_60()
+        {
+            var pythagoreanProduct = E9_Special_Pythagorean_triplet.PythagoreanProduct(12);
+
+            Assert.Equal(60, pythagoreanProduct);
+        }
+
+        [Fact]
+        public void SpecialPythagoreanTriplet_31875000()
+        {
+            var pythagoreanProduct = E9_Special_Pythagorean_triplet.PythagoreanProduct(1000);
+
+            Assert.Equal(31875000, pythagoreanProduct);
+        }
+    }
+}
