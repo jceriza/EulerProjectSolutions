39642632164602b0c5bba1f886cc89a9dc6feb3c
Joaquín Ceriza <jceriza@gmail.com>
2018-06-06 13:23:26 +0200

    Problem 28 solved

---
+++ Exercises/E028_Number_spiral_diagonals.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Exercises
+{
+    public static class E028_Number_spiral_diagonals
+    {
+        public static int SumDiagonals(int size)
+        {
+            var topLeftDiagonal = new List<int> { 1 };
+            var bottomLeftDiagonal = new List<int> { };
+
+            var lastTopLeftDiagonalNumberInserted = 1;
+            var lastBottomLeftDiagonalNumberInserted = 1;
+
+            for (int i = 0, j = 2, k = 4; i < size / 2; i++, k += 4)
+            {
+                for (int l = 0; l < 2; l++)
+                {
+                    lastTopLeftDiagonalNumberInserted += j;
+                    topLeftDiagonal.Add(lastTopLeftDiagonalNumberInserted);
+
+                    j += 2;
+
+                    lastBottomLeftDiagonalNumberInserted += k;
+                    bottomLeftDiagonal.Add(lastBottomLeftDiagonalNumberInserted);
+                }
+            }
+
+            return topLeftDiagonal.Sum() + bottomLeftDiagonal.Sum();
+        }
+    }
+}
---
+++ Exercises_Tests/E028_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E028_Tests
+    {
+        [Fact]
+        public void DiagonalsSum_5_101()
+        {
+            Assert.Equal(
+                101,
+                E028_Number_spiral_diagonals.SumDiagonals(5));
+        }
+
+        [Fact]
+        public void DiagonalsSum_1001_669171001()
+        {
+            Assert.Equal(
+                669171001,
+                E028_Number_spiral_diagonals.SumDiagonals(1001));
+        }
+    }
+}
