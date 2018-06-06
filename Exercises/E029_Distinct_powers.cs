73d30a26872a18cb86e5c1e9f44c172c8c289984
Joaquín Ceriza <jceriza@gmail.com>
2018-06-06 14:01:06 +0200

    Problem 29 solved

---
+++ Exercises/E029_Distinct_powers.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using Utilities;
+
+namespace Exercises
+{
+    public static class E029_Distinct_powers
+    {
+        public static HashSet<string> Sequence(int from, int to)
+        {
+            var sequence = new HashSet<string>();
+
+            for (int a = from; a <= to; a++)
+            {
+                var aLargeNumber = new LargeNumber(a);
+                var aggregate = new LargeNumber(a);
+
+                for (int i = 1; i < from; i++)
+                {
+                    aggregate *= aLargeNumber;
+                }
+
+                sequence.Add(aggregate.ToString());
+
+                for (int b = from + 1; b <= to; b++)
+                {
+                    aggregate *= aLargeNumber;
+                    sequence.Add(aggregate.ToString());
+                }
+            }
+
+            return sequence;
+        }
+    }
+}
---
+++ Exercises_Tests/E029_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Exercises;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E029_Tests
+    {
+        [Fact]
+        public void DistinctPowersSequence_2_5()
+        {
+            Assert.Equal(
+                new HashSet<string>
+                {
+                    "4", "8", "9", "16", "25", "27", "32", "64", "81", "125", "243", "256", "625", "1024", "3125"
+                },
+                E029_Distinct_powers.Sequence(2, 5));
+        }
+
+        [Fact]
+        public void DistinctPowersSequence_2_100()
+        {
+            Assert.Equal(
+                9183,
+                E029_Distinct_powers.Sequence(2, 100).Count);
+        }
+    }
+}
