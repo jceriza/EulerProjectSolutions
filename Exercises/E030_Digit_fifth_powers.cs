bdf4b3496e5d60292ef293bd6ff965ac6c2195cb
Joaquín Ceriza <jceriza@gmail.com>
2018-06-07 09:39:59 +0200

    Problem 30 solved

---
+++ Exercises/E030_Digit_fifth_powers.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Utilities;
+
+namespace Exercises
+{
+    public static class E030_Digit_fifth_powers
+    {
+        public static int SumNthPowersOfTheirDigits(int n)
+        {
+            return new DigitPowers(n).GetPowers().Sum();
+        }
+    }
+}
---
+++ Exercises_Tests/E030_Tests.cs
@@ -0,0 +1,25 @@
+﻿using Exercises;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E030_Tests
+    {
+        [Fact]
+        public void SumNthPowersOfTheirDigits_4()
+        {
+            Assert.Equal(
+                19316,
+                E030_Digit_fifth_powers.SumNthPowersOfTheirDigits(4));
+        }
+
+        [Fact]
+        public void SumNthPowersOfTheirDigits_5()
+        {
+            Assert.Equal(
+                443839,
+                E030_Digit_fifth_powers.SumNthPowersOfTheirDigits(5));
+        }
+    }
+}
