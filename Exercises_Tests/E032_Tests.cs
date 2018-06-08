5a86e3b46a4334c454d4828d0bec51ccc897d3f4
Joaquín Ceriza <jceriza@gmail.com>
2018-06-08 19:26:54 +0200

    Problem 32 solved

---
+++ Exercises_Tests/E032_Tests.cs
@@ -0,0 +1,17 @@
+﻿using Exercises;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E032_Tests
+    {
+        [Fact]
+        public void PandigitalProductsSum()
+        {
+            Assert.Equal(
+                45228,
+                E032_Pandigital_products.ProductsSum());
+        }
+    }
+}
