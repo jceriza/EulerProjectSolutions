0c94843b8cda7ca9ce1591550ba0121dab529598
Joaquín Ceriza <jceriza@gmail.com>
2018-06-12 17:43:58 +0200

    Problem 37 solved

---
+++ Exercises/E037_Truncatable_primes.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+
+namespace Exercises
+{
+    public class E037_Truncatable_primes
+    {
+        private static bool IsTruncatablePrime(long num)
+        {
+            var leftToRight = 1;
+            var rightToLeft = 10;
+            var aux = num;
+
+            while (aux > 10)
+            {
+                aux /= 10;
+                leftToRight *= 10;
+            }
+
+            while (leftToRight > 1)
+            {
+                if (!(PrimeNumbers.IsPrimeNumber(num / leftToRight)
+                    && PrimeNumbers.IsPrimeNumber(num % rightToLeft)))
+                {
+                    return false;
+                }
+
+                leftToRight /= 10;
+                rightToLeft *= 10;
+            }
+
+            return true;
+        }
+        
+        public static long TruncatablePrimesSum()
+        {
+            var sum = 0L;
+            var count = 0;
+
+            foreach (var prime in PrimeNumbers.PrimesGeneratorFrom2().SkipWhile(p => p < 10))
+            {
+                if (IsTruncatablePrime(prime))
+                {
+                    sum += prime;
+
+                    if (++count == 11) break;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Exercises_Tests/E037_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E037_Tests
+    {
+        [Fact]
+        public void TruncatablePrimesSum()
+        {
+            Assert.Equal(
+                748317,
+                E037_Truncatable_primes.TruncatablePrimesSum());
+        }
+    }
+}
