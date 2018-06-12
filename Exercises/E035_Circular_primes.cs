a1fe4f390b195f83c47248018315aa64cf14eb79
Joaquín Ceriza <jceriza@gmail.com>
2018-06-12 16:27:10 +0200

    Problem 35 solved

---
+++ Exercises/E035_Circular_primes.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Utilities;
+
+namespace Exercises
+{
+    public static class E035_Circular_primes
+    {
+        private static IEnumerable<int> NumRotations(int num)
+        {
+            if (num < 10) yield return num;
+
+            else
+            {
+                var nums = new Queue<int>();
+
+                while (num > 0)
+                {
+                    var numToAdd = num % 10;
+                    nums.Enqueue(num % 10);
+                    num /= 10;
+                }
+
+                var i = 0;
+                do
+                {
+                    var mult = 1;
+                    var currentNumber = 0;
+                    foreach (var number in nums)
+                    {
+                        currentNumber += number * mult;
+                        mult *= 10;
+                    }
+
+                    yield return currentNumber;
+
+                    nums.Enqueue(nums.Dequeue());
+                } while (i++ < nums.Count - 1);
+            }
+        }
+
+        public static int NumCircularPrimesBelowN(int n)
+        {
+            var circularPrimes = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (NumRotations(i).All(rotation => PrimeNumbers.IsPrimeNumber(rotation)))
+                {
+                    circularPrimes++;
+                }
+            }
+
+            return circularPrimes;
+        }
+    }
+}
---
+++ Exercises_Tests/E035_Tests.cs
@@ -0,0 +1,24 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E035_Tests
+    {
+        [Fact]
+        public void CircularPrimesUnder_100_13()
+        {
+            Assert.Equal(
+                13,
+                E035_Circular_primes.NumCircularPrimesBelowN(100));
+        }
+
+        [Fact]
+        public void CircularPrimesUnder_1000000_X()
+        {
+            Assert.Equal(
+                55,
+                E035_Circular_primes.NumCircularPrimesBelowN(1_000_000));
+        }
+    }
+}
