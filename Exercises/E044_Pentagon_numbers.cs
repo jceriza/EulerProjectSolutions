9cb932a171553fb1c08be68d3a4f79e7df6d1a23
Joaquín Ceriza <jceriza@gmail.com>
2018-06-15 14:48:35 +0200

    Problem 44 solved

---
+++ Exercises/E044_Pentagon_numbers.cs
@@ -0,0 +1,12 @@
+﻿using Utilities;
+
+namespace Exercises
+{
+    public static class E044_Pentagon_numbers
+    {
+        public static long MinimisedPentagonalNumbersDifference()
+        {
+            return PentagonNumbers.MinimisedPentagonalNumbersDifference();
+        }
+    }
+}
---
+++ Exercises_Tests/E044_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E044_Tests
+    {
+        [Fact]
+        public void SubstringDivisibility()
+        {
+            Assert.Equal(
+                5482660,
+                E044_Pentagon_numbers.MinimisedPentagonalNumbersDifference());
+        }
+    }
+}
