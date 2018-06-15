5bcd25d5ff4b794fdeaad987359748fa3a21a998
Joaquín Ceriza <jceriza@gmail.com>
2018-06-15 17:36:54 +0200

    Problem 46 solved

---
+++ Exercises/E046_Goldbach_other_conjecture.cs
@@ -0,0 +1,47 @@
+﻿using System.Linq;
+using Utilities;
+
+namespace Exercises
+{
+    public static class E046_Goldbach_other_conjecture
+    {
+        private static bool CompliesWithGoldbachConjecture(int number)
+        {
+            var primes = PrimeNumbers.PrimesGeneratorFrom2().TakeWhile(prime => prime <= number);
+
+            foreach (var prime in primes)
+            {
+                var rest = number - prime;
+
+                var i = 1;
+                int mult;
+
+                do
+                {
+                    mult = 2 * (i * i++);
+                } while (mult < rest);
+
+                if (mult == rest) return true;
+            }
+
+            return false;
+        }
+
+        private static bool IsComposite(int number)
+        {
+            return !PrimeNumbers.IsPrimeNumber(number);
+        }
+
+        public static long SmallestOddNotGoldbach()
+        {
+            int number = 1;
+            do
+            {
+                number += 2;
+                while (!IsComposite(number)) number += 2;
+            } while (CompliesWithGoldbachConjecture(number));
+
+            return number;
+        }
+    }
+}
---
+++ Exercises_Tests/E046_Tests.cs
@@ -0,0 +1,16 @@
+﻿using Exercises;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E046_Tests
+    {
+        [Fact]
+        public void GoldbachOtherConjecture()
+        {
+            Assert.Equal(
+                5777,
+                E046_Goldbach_other_conjecture.SmallestOddNotGoldbach());
+        }
+    }
+}
