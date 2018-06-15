60740aed35cc01ab3ae010b0c65cdaae8f71e940
Joaquín Ceriza <jceriza@gmail.com>
2018-06-15 20:58:25 +0200

    Problem 47 solved

---
+++ Exercises/E047_Distinct_primes_factors.cs
@@ -0,0 +1,57 @@
+﻿namespace Exercises
+{
+    public static class E047_Distinct_primes_factors
+    {
+        public static bool HasNPrimeFactors(int number, int n)
+        {
+            if (number % 2 == 0)
+            {
+                n--;
+                do
+                {
+                    number /= 2;
+                } while (number % 2 == 0);
+            }
+
+            for (int i = 3; n >= 0 && number > 2; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    if (--n < 0) return false;
+
+                    do
+                    {
+                        number /= i;
+                    } while (number % i == 0);
+                }
+            }
+
+            return n == 0;
+        }
+
+        public static int FirstNNumbersWithNPrimeFactors(int n)
+        {
+            int number = -1;
+            var quantity = 0;
+
+            for (int i = n; quantity < n; i += n)
+            {
+                if (HasNPrimeFactors(i, n))
+                {
+                    quantity = 0;
+
+                    for (int j = i - n + 1; j < i + n && quantity < n; j++)
+                    {
+                        if (HasNPrimeFactors(j, n))
+                        {
+                            if (quantity++ == 0) number = j;
+                        }
+                        else quantity = 0;
+                    }
+                }                
+            }            
+
+            return number;
+        }
+    }
+}
---
+++ Exercises_Tests/E047_Tests.cs
@@ -0,0 +1,33 @@
+﻿using Exercises;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E047_Tests
+    {
+        [Fact]
+        public void First2NumbersWith2PrimeFactors()
+        {
+            Assert.Equal(
+                14,
+                E047_Distinct_primes_factors.FirstNNumbersWithNPrimeFactors(2));
+        }
+
+        [Fact]
+        public void First3NumbersWith3PrimeFactors()
+        {
+            Assert.Equal(
+                644,
+                E047_Distinct_primes_factors.FirstNNumbersWithNPrimeFactors(3));
+        }
+
+        [Fact]
+        public void First4NumbersWith4PrimeFactors()
+        {
+            Assert.Equal(
+                134043,
+                E047_Distinct_primes_factors.FirstNNumbersWithNPrimeFactors(4));
+        }
+    }
+}
