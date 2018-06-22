ef7bce61e6c36d9566fcaf1a01763681aa4d14f4
Joaquín Ceriza <jceriza@gmail.com>
2018-06-22 16:25:47 +0200

    Problem 48 solved

---
+++ Exercises/E048_Self_powers.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utilities;
+
+namespace Exercises
+{
+    public static class E048_Self_powers
+    {
+        public static LargeNumber SelfPowerSeriesSumUntilN(int n)
+        {
+            var sum = new LargeNumber(0);
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += LargeNumber.Pow(i, i);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Exercises_Tests/E048_Tests.cs
@@ -0,0 +1,28 @@
+﻿using Exercises;
+using System.Collections.Generic;
+using Utilities;
+using Xunit;
+
+namespace Exercises_Tests
+{
+    public class E048_Tests
+    {
+        [Fact]
+        public void SelfPowerSeriesSumUntil10()
+        {
+            Assert.Equal(
+                new LargeNumber(10405071317),
+                E048_Self_powers.SelfPowerSeriesSumUntilN(10));
+        }
+
+        [Fact]
+        public void SelfPowerSeriesSumUntil1000()
+        {
+            var numberString = E048_Self_powers.SelfPowerSeriesSumUntilN(1000).ToString();
+
+            Assert.Equal(
+                "9110846700",
+                numberString.Substring(numberString.Length - 10));
+        }
+    }
+}
